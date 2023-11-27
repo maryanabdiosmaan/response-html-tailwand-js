9a14f6e2f808269e3aaa3750d32f8c256bb0d6e6
maryanabdiosman <abdimaryan41@gmail.com>
2023-11-27 08:56:19 +0100

    this is response html tailwand and js

---
+++ main.js
@@ -0,0 +1,13 @@
+const iconButton =document.querySelector("#iconButton")
+const links =document.querySelector("#links")
+const closeButton =document.querySelector("#closeButton")
+iconButton .addEventListener("click",function(){
+    links.style.display = "flex"
+    closeButton.style.display = "block"
+    iconButton.style.display = "none"
+} )
+closeButton .addEventListener("click",function(){
+    links.style.display = "none"
+    closeButton.style.display = "none"
+    iconButton.style.display = "block"
+} )
